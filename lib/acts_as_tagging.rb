f53cf1f4b20567d91096b0d09d6d622d42a87b4d
Tyralion <piliaiev@gmail.com>
2011-12-20 16:26:47 +0600

    Удален несуществующий файл в require.

--- lib/acts_as_tagging.rb
+++ lib/acts_as_tagging.rb
@@ -3,6 +3,5 @@ require 'acts_as_tagging/base'
 require 'acts_as_tagging/document'
 require 'acts_as_tagging/tag_list'
 require 'acts_as_tagging/tag'
-require 'acts_as_tagging/owner'
 
 Mongoid::Document::ClassMethods.send(:include, ActsAsTagging::Base)
