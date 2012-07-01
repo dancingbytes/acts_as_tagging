ed4e4e11593914b87b931cac0e6b574ba1be49c3
Tyralion <piliaiev@gmail.com>
2012-07-01 16:30:29 +0600

    Адаптация проекта под mongoid 3.

--- lib/acts_as_tagging/mongoid/tag.rb
+++ lib/acts_as_tagging/mongoid/tag.rb
@@ -9,14 +9,33 @@ module ActsAsTagging
     field :context_type
     field :context_id
     
-    index(
-      [
-        [ :name,          Mongo::ASCENDING ],
-        [ :context_type,  Mongo::ASCENDING ],
-        [ :context_id,    Mongo::ASCENDING ]
-      ],
-      unique: true
-    )
+    # For mongoid 2.4
+    unless (::Mongoid::VERSION =~ /\A2.4/).nil?
+
+      index(
+        [
+          [ :name,          Mongo::ASCENDING ],
+          [ :context_type,  Mongo::ASCENDING ],
+          [ :context_id,    Mongo::ASCENDING ]
+        ],
+        unique: true
+      )
+
+    else
+
+      # For mongoid 3
+
+      index({
+        
+        name: 1,
+        context_type: 1,
+        context_id:   1
+
+      }, {  
+        unique: true
+      })
+    
+    end  
 
   end # Tag
 
